7977b2273f5e59746561ac13a4dbb82adc1b0983
Kacper Lutynski <kacper.lutynski@devscntr.com>
2022-07-15 16:09:58 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const fetchHandler = () => {
+    axios.get('https://dummyjson.com/products')// i left here
+  }
+
+  const DUMMY_DATA = {
+    products: [
+      {
+        id: 1,
+        title: "iPhone 9",
+      },
+      {
+        id: 2,
+        title: "iPhone 8",
+      },
+      {
+        id: 3,
+        title: "iPhone 7",
+      }
+    ]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="App" onSubmit={fetchHandler}>
+      <input type="text" placeholder='Search...' />
+      <button type='submit'>Fetch</button>
+
+      {DUMMY_DATA.products.map((prod) => {
+        return <p>{prod.title}</p>
+      })}
+
+    </form>
   );
 }
 
